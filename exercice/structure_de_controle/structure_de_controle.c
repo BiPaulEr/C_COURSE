69ae1b9cd3e4a260be77186edde9af4a649bb39d
PaulErnest <PaulErMartin@yahoo.fr>
2025-12-22 16:48:48 +0100

    exo

---
+++ exercice/structure_de_controle/structure_de_controle.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10, y = 3, note = 14, a = 7, b = 10;
+    int max;
+
+    if (********) {
+        printf("x est plus grand que 5\n");
+    }
+
+    if (******) {
+        printf("y est grand\n");
+    } *****{
+        printf("y est petit ou égal à 5\n");
+    }
+
+    if (****6) {
+        printf("Note %d : Mention Très bien\n", note);
+    } else if (note >= 12) {
+        printf("Note %d : Mention Bien\n", note);
+    } else {
+        printf("Note %d : À améliorer\n", note);
+    }
+
+    max = *********
+    printf("Le maximum entre %d et %d est %d\n", a, b, max);
+    return 0;
+}
