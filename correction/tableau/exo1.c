48ac0acf69f78c9133b24adfddc8c2be68a633ca
PaulErnest <PaulErMartin@yahoo.fr>
2026-03-09 10:38:34 +0100

    demo control.c

---
+++ correction/tableau/exo1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int tab[10];
+
+    for (int i = 0; i < 10; i++){
+        tab[i] = i;
+    }
+    for (int i = 0; i < 10; i++){
+        printf("%d", tab[i]);
+    }
+}
